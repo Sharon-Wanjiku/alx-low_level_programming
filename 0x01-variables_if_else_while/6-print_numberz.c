2ff077f40d91d5b6cbffec7ad287551620004f0f
Sharon-Wanjiku <sharonwanjiku.a@gmail.com>
2022-12-19 22:58:07 -0800

    Smile in the mirror

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - prints all single digit numbers of base 10 starting from 0
- *
+ * main-entry point.
+ * Description- prints all single digit numbers of base 10 starting from 0
  * followed by a new line
- *
  * return: always 0
  */
 int main(void)
